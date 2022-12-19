abdcaa349a0cc020416d8f3bd78066639afeb21d
mhchoi <alsgp0877@naver.com>
2022-12-20 00:06:54 +0900

    js 기본문법 배우는중 2

--- app.js
+++ app.js
@@ -94,11 +94,27 @@ console.log("plusResult : " +  plusResult);
 const minusResult = calculatorReturn.minus(plusResult,1);
 console.log("minusResult : " +  minusResult);
 
-const age = prompt("how old are you?");//아주 옛날 방법! 
+const age = parseInt(prompt("how old are you?"));//아주 옛날 방법! 
+console.log(typeof age);//변수의 타입을 알 수 있음. 
 
-parseInt();
+console.log(isNaN(age));//매개변수가 숫자인지 검사하는 함수. Not a Number
+//굳이 쓰는이유? parseInt(숫자)를 써주면 이상적이지만 parseInt(;멍;람) 이렇게 쓸수 도 있는것 그러면 NaN을 표출한다.
+if(isNaN(age) || age < 0){
+  console.log("Pleas write a real positive number!");
+}else if(age<18){
+  console.log("You are too young.");
+}else if(age >=18 && age<=50){
+  console.log("You can drink");
+}else if(age >50 && age<=80){
+  console.log("You should exercise");
+}else if(age === 100){
+  console.log("You are wise");
+}else if(age >80){
+  console.log("You can do whatever you want.");
+}
 
-console.log(typeof age);
+
+
 
 
 
