afe3a272dfc49299c7c11e224e9b47757bb41729
mhchoi <alsgp0877@naver.com>
2022-12-29 22:46:47 +0900

    FORM와 INPUT 태그 토글 기능

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden";
+
+//반복되는 string 들은 대문자 변수로 저장해놓는게 좋다. 오탈자 방지
+const HIDDEN_CLASSNAME = "hidden"; 
+const USERNAME_KEY = "username";
+
+function paintGreetings(username){
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+}
 
 function onLoginSubmit(event){
   event.preventDefault();
@@ -10,10 +18,20 @@ function onLoginSubmit(event){
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   //greeting.innerText  = "Hello" + username;
-  greeting.innerText  = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY,username); //우리가 dom api를 사용할 수 있는것 처럼 어디엔가 메소드를 저장해두는곳이있고 또 가져와서 쓸수 있는 곳이 있다.
+  paintGreetings(username);
 
 }
 
 loginForm.addEventListener("submit",onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if(savedUsername === null){
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+  //show the greetings
+  paintGreetings(savedUsername);
+}
 
