a01255e7220dbd07a66b8633ebe145bf99c522ce
mhchoi <alsgp0877@naver.com>
2023-01-01 23:20:30 +0900

    시계를 만들기 위한 시간 자릿수 맞추기

--- clock.js
+++ clock.js
@@ -4,7 +4,11 @@ const clock = document.querySelector("h2#clock");
 //자바스크립트에선 해당 기능을 제공한다. 쓸때가 많기 때문에
 function getClock(){
   const date = new Date();
-  clock.innerText=`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = String(date.getHours()).padStart(2,"0");
+  const minutes = String(date.getMinutes()).padStart(2,"0");
+  const seconds = String(date.getSeconds()).padStart(2,"0");
+
+  clock.innerText=`${hours}:${minutes}:${seconds}`;
 }
 getClock();
 setInterval(getClock,1000);
