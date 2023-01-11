cd5ff81b77b79ed9d3dc9eb19e65dd896fab8e3f
mhchoi <alsgp0877@naver.com>
2023-01-11 22:55:41 +0900

    filter로 데이터 제거하기 1

--- todo.js
+++ todo.js
@@ -20,10 +20,11 @@ function deleteToDo(event){
   const li = event.target.parentElement;//target은 클릭된 HTML element
   li.remove();
 }
-function paintToDo(newTodo){
+function paintToDo(newTodoObj){
   const li = document.createElement("li");
+  li.id = newTodoObj.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodoObj.text;
   li.appendChild(span);
 
   const button = document.createElement("button");
@@ -37,19 +38,42 @@ function handleToDoSubmit(event){
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);//할일을 하나씩 submit할때마다 배열에 쌓이고 새로고침하면 사라짐. submit할때 새로고침하는걸 막았으니 가능함.나는 submit할때 새로고침되면 글이 사라지는데 어떻게 되나 싶었네 ㅋㅋ
-  paintToDo(newTodo);
+  //삭제를 위해서는 어떤 걸 삭제하는지에 대한 고유 ID가 있어야 접근가능하다.
+  //객체로 만들어서 아이디와 값을 같이 넣어준다.
+  const newTodoObj = {
+    text : newTodo,
+    id : Date.now(),
+  }
+  toDos.push(newTodoObj);//할일을 하나씩 submit할때마다 배열에 쌓이고 새로고침하면 사라짐. submit할때 새로고침하는걸 막았으니 가능함.나는 submit할때 새로고침되면 글이 사라지는데 어떻게 되나 싶었네 ㅋㅋ
+  paintToDo(newTodoObj);
   saveToDos();
 
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
 
-
+//그냥 순서대로 내려오다보니 여기가 실행되서 DB에 있는 데이터가 나오는듯?
 const savedToDos = localStorage.getItem(TODOS_KEY);
-if(saveToDos !== null){
-  const parsedToDos = JSON.parse(saveToDos);  //string을 array로 바꿔준다.
+if(savedToDos !== null){
+  const parsedToDos = JSON.parse(savedToDos);  //string을 array로 바꿔준다.
   toDos = parsedToDos;
   //forEach는 item을 제공해준다.
   parsedToDos.forEach(paintToDo);
 }
+
+
+//parsedToDos.forEach(paintToDo); 코드처럼 배열의 각 요소마다 함수를 달아주는것을 할것이다 그것이 filter
+//filter은 배열에서 요소를 진짜 지우는것이 아니라 지우고 싶은 요소를 빼고 새 배열을 만든다.
+//item을 지우는게 아니라 item을 제외한다.
+//직동방법
+//function을 [1,2,3,4] 배열 요소의 갯수만큼 4번을 부르고 false를 리턴하면 그 요소는 새 배열에 포함되지 않는다. true는 반대
+[1,2,3,4].filter(functions());
+functions(1) t
+functions(2) t
+functions(3) f
+functions(4) t
+//3빼고 1,2,4만 유지한다.
+//#7.7 4:45
+
+
+
